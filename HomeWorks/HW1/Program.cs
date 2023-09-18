06f6813697321b0f34370e01a707e165480c5631
Anna Oleksevich <oleksanna@mail.ru>
2023-09-18 23:07:58 +0800

    HomeWorks

--- HomeWorks/HW1/Program.cs
+++ HomeWorks/HW1/Program.cs
@@ -4,8 +4,18 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-
-
+// System.Console.WriteLine("Ввдеите число А: ");
+// int numA = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine("Введите число В: ");
+// int numB = Convert.ToInt32(System.Console.ReadLine());
+// if(numA > numB)
+// {
+   //  System.Console.WriteLine($"a = {numA}, b = {numB} -> max = {numA}");
+// }
+// else
+// {
+    // System.Console.WriteLine($"a = {numA}, b = {numB} -> max = {numB}");
+// }
 
 
 
@@ -14,8 +24,20 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+// System.Console.WriteLine("Ввдеите число А: ");
+// int numA = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine("Введите число В: ");
+// int numB = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine("Ввдеите число C: ");
+// int numC = Convert.ToInt32(System.Console.ReadLine());
 
+// int max = numA;
 
+// if(numA > max) max = numA;
+// if(numB > max) max = numB;
+// if(numC > max) max = numC;
+
+// System.Console.WriteLine($"{numA}, {numB}, {numC} -> {max}");
 
 
 
@@ -24,8 +46,17 @@
 // -3 -> нет
 // 7 -> нет
 
+// System.Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(System.Console.ReadLine());
 
-
+// if (num%2 == 0)
+// {
+  // System.Console.WriteLine($"{num} -> да");  
+// }
+// else
+// {
+    // System.Console.WriteLine($"{num} -> нет"); 
+// }
 
 
 
@@ -33,5 +64,11 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+System.Console.WriteLine("Введите число N: ");
+int numN = Convert.ToInt32(System.Console.ReadLine());
+int i = 1;
 
-
+while(i <= numN)
+{
+    System.Console.WriteLine(i++);
+}
