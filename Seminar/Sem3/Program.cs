98a6fda5498091bd2fed8949222ae8118aee16cc
Anna Oleksevich <oleksanna@mail.ru>
2023-10-02 20:05:00 +0800

    Seminar

--- Seminar/Sem3/Program.cs
+++ Seminar/Sem3/Program.cs
@@ -64,3 +64,60 @@
 
 
 
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+// void coordQuarter(int quarter)
+// {
+    // if (quarter == 1)
+    // {
+        // System.Console.WriteLine("x > 0 и y > 0");
+    // }
+    // else if (quarter == 2)
+    // {
+        // System.Console.WriteLine("x < 0 и y > 0");
+    // }
+    // else if (quarter == 3)
+    // {
+        // System.Console.WriteLine("x < 0 и y < 0");
+    // }
+    // else if (quarter == 4)
+    // {
+        // System.Console.WriteLine("x > 0 и y < 0");
+    // }
+    // else
+    // {
+        // System.Console.WriteLine("Error!");
+    // }
+// }
+
+// System.Console.Write("Input quarter: ");
+// int q = Convert.ToInt32(Console.ReadLine());
+// coordQuarter(q);
+
+
+
+// Напишите программу , которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+// A(3,6); B(2,1) -> 5,09
+// A(7,-5); B(1,-1) -> 7,21
+
+
+// double Distance(double xa, double ya, double xb, double yb)
+// {
+    // return Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, ));
+// }
+
+// System.Console.WriteLine("Напишите значение координат: ");
+// System.Console.Write("Координата x точки А: ");
+// double xa = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Координата y точки А: ");
+// double ya = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Координата x точки B: ");
+// double xb = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Координата y точки B: ");
+// double yb = Convert.ToDouble(Console.ReadLine());
+
+// System.Console.WriteLine($"A({xa},{xb}); B({xb},{yb}) -> {Math.Round(Distance(xa, ya, xb, yb), 2)}");
