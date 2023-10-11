c7b8d6f5adc999ca5c9917d57fa8f4125a8717a1
Anna Oleksevich <oleksanna@mail.ru>
2023-10-11 20:22:50 +0800

    Seminar

--- Seminar/Sem7/Program.cs
+++ Seminar/Sem7/Program.cs
@@ -102,6 +102,63 @@
 // 8 4 2 4
 
 
+// int InputInteger(string message)
+// {
+//     Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+
+// int[,] FillArray(int rows, int columns, int min, int max)
+// {
+//     Random rand = new Random();
+//     int [,] result = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//         for (int j = 0; j < columns; j++)
+//         {
+//             result[i, j] = rand.Next(min, max + 1);
+//         }
+//     return result;
+// }
+
+// void ShowMatrix (int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j], 3}");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine();
+//     }
+// }
+// void ChangeArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//          for (int j = 0; j < array.GetLength(1); j++)
+//          {
+//             if (i % 2 == 0 && j % 2 == 0)
+//             {
+//                 array[i, j] *= array[i, j];
+//             }
+//          }
+//     }
+// }
+
+// int rows = InputInteger("Введите количество строк: ");
+// int columns = InputInteger("Введите количество стоблцов: ");
+// int min = InputInteger("Введите минимальное значение диапазона: ");
+// int max = InputInteger("Введите максимальное значение диапазона: ");
+// int[,] array = FillArray(rows, columns, min, max);
+// ShowMatrix(array);
+// ChangeArray(array);
+// Console.WriteLine("Измененный массив:");
+// ShowMatrix(array);
+
+
 
 // --------------------------------------------------------------------------------------------------
 
@@ -113,6 +170,33 @@
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
+int InputInteger(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+void Fill2DArray(int[,] array, int minValue, int maxValue)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = random.Next(minValue, maxValue + 1);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
 
 int FindMinimum(int[,] array)
 {
@@ -123,7 +207,7 @@ int FindMinimum(int[,] array)
 }
 
 
-int GetSumMainDiagonal(int[,] array)
+int GetSumMainDiagonal(int[,] array, int rows, int columns)
 {
     int minSize = FindMinimum(array);
     int sum = 0;
@@ -136,8 +220,15 @@ int GetSumMainDiagonal(int[,] array)
     return sum;
 }
 
-
-
+int rows = InputInteger("Введите количество строк: ");
+int columns = InputInteger("Введите количество стоблцов: ");
+int minValue = InputInteger("Введите минимальное значение диапазона: ");
+int maxValue = InputInteger("Введите максимальное значение диапазона: ");
+int[,] arr = Create2DArray(rows, columns);
+Fill2DArray(arr, minValue, maxValue);
+Print2DArray(arr);
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма по главной диагонали: {GetSumMainDiagonal(arr, rows, columns)}");
 
 
 
